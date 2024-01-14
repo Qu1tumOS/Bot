6e6d313035188fa6c10a34628c8b4b2eb9789285
Qu1tum <vladimirzagotovkin@yandex.ru>
2024-01-14 23:52:14 +0300

    изменен вывод пар

--- handlers/menu_handlers/admin_panel/open_panel.py
+++ handlers/menu_handlers/admin_panel/open_panel.py
@@ -3,6 +3,10 @@ from aiogram.types import CallbackQuery
 
 from parser.pars import url_groups_update
 
+from parser.pars import group_par, date
+from parser.lexicon_pars import print_day
+
+
 from keyboards.keyboard_creator import create_inline_kb
 
 from DataBase.db_connect import *
@@ -17,6 +21,7 @@ async def open_admin_panel(callback: CallbackQuery):
                 text='ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ',
                 reply_markup=create_inline_kb(1,
                                               update_url_group='Обновить группы',
+                                              week_pars='пары на послезавтра',
                                               profile='Назад')
             )
     await callback.answer()
@@ -26,3 +31,17 @@ async def open_admin_panel(callback: CallbackQuery):
 async def open_admin_panel(callback: CallbackQuery):
     url_groups_update()
     await callback.answer('Succes!')
+
+@router.callback_query(F.data == 'week_pars')
+async def week_pars(callback: CallbackQuery):
+    user = session.query(User).filter(User.tg_id==callback.from_user.id).first()
+
+    today_next = date(2)
+    request_site = group_par(user.group)
+    await callback.message.edit_text(
+        text=f'`{print_day(today_next, request_site, user.subgroup)}`',
+        parse_mode='MarkdownV2',
+        reply_markup=create_inline_kb(2,
+                                      menu_button='Назад',
+                                      update_tomorrow='Обновить')
+    )
--- parser/lexicon_pars.py
+++ parser/lexicon_pars.py
@@ -11,7 +11,7 @@ days = {
 }
 
 
-def print_day(rasp_date, list_days, subgroup):
+def print_da2y(rasp_date, list_days, subgroup):
     subgroup -= 1
     day = datetime.datetime.strptime(rasp_date, "%d.%m.%Y")
 
@@ -32,3 +32,35 @@ def print_day(rasp_date, list_days, subgroup):
     if week != 'Вс':
         return pars
     return 'единственный выходной 🥳'
+
+def print_day(rasp_date, list_days, subgroup):
+    subgroup -= 1
+
+    day = datetime.datetime.strptime(rasp_date, "%d.%m.%Y")
+    date = f"""{day.strftime('%d.%m.%Y')}"""
+    week = f"""{days[day.strftime('%w')]}"""
+
+    pars = list_days[date]
+    tabs = 22
+
+    output = [f'{date[:-5].rjust(15, " ")} {week.ljust(tabs-12, " ")}']
+
+    descript = 'ПМ.02 ПМ.05 ОП.01 ОП.02 ОП.03 ОП.04 ОП.05 ОГСЭ.04 ОГСЭ.03'
+
+
+    for i in pars:
+        lesson = i[subgroup][0].split(' ', 1)
+        print(lesson)
+        cab = i[subgroup][1]
+
+        if (lesson[0] != '') and (lesson[0] in descript):
+            para = f'''{lesson[1].ljust(tabs, " ")} {cab}'''
+        else:
+            para = f'''{lesson[0 ]+ ' '}{lesson[1].ljust(tabs, " ")}{cab}'''
+
+        output.append(para)
+
+    output = '\n'.join(output)
+
+
+    return output
