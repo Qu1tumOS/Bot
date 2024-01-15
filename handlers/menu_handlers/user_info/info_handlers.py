1d329c65ec00dee4a512f6ff1b21a395f22a2333
Qu1tum <vladimirzagotovkin@yandex.ru>
2024-01-15 22:17:17 +0300

    фикс профиля (не работал хендлер при нижнем подчеркивании)

--- handlers/menu_handlers/user_info/info_handlers.py
+++ handlers/menu_handlers/user_info/info_handlers.py
@@ -12,14 +12,20 @@ router = Router()
 @router.callback_query(F.data == 'user_info')
 async def user_info(callback: CallbackQuery):
     user = session.query(User).filter(User.tg_id==callback.from_user.id).first()
+
+    user.name = callback.from_user.username
+    user.user_name = callback.from_user.first_name
+    
+    name = str(user.name).replace('<', '').replace('>', '').replace('&', '')
+    user_name = str(user.user_name).replace('<', '').replace('>', '').replace('&', '')
+
     await callback.message.edit_text(
-        text=f'@{user.name}\n'\
-             f'\n'\
-             f'`Имя      {str(user.user_name).rjust(15, " ")}`\n'\
-             f'`Техникум {str(user.collage).rjust(15, " ")}`\n'\
-             f'`Группа   {str(user.group).rjust(15, " ")}`\n'\
-             f'`Подгруппа{str(user.subgroup).rjust(15, " ")}`',
-        parse_mode='MarkdownV2',
+        text=f'@{str(name)}\n\n'\
+             f'<code>Имя      {str(user_name).rjust(15, " ")}</code>\n'\
+             f'<code>Техникум {str(user.collage).rjust(15, " ")}</code>\n'\
+             f'<code>Группа   {str(user.group).rjust(15, " ")}</code>\n'\
+             f'<code>Подгруппа{str(user.subgroup).rjust(15, " ")}</code>',
+        parse_mode='HTML',
         reply_markup=create_inline_kb(2,
                                       edit_profile='Изменить',
                                       delete_profile='Удалить',
