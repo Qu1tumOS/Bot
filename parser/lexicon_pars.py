c07dc808178345fe7cd0be57e2c6ef26accb8096
Qu1tumOS <134674840+Qu1tumOS@users.noreply.github.com>
2024-01-23 13:18:39 +0300

    Update lexicon_pars.py

--- parser/lexicon_pars.py
+++ parser/lexicon_pars.py
@@ -23,14 +23,14 @@ def print_day(rasp_date, list_days, subgroup):
 
     output = [f'{date[:-5].rjust(15, " ")} {week.ljust(tabs-12, " ")}']
 
-    descript = 'ПМ.02 ПМ.05 ОП.01 ОП.02 ОП.03 ОП.04 ОП.05 ОГСЭ.04 ОГСЭ.03 ОУД.14 ОУД.04 ОУД.10 ОУД.11 ОУД.03 ОУД.07 ОП.11 ЕН.03'
+    descript = 'ПМ. ОП. ОГСЭ. ЕН.'
 
 
     for i in pars:
         lesson = i[subgroup][0].split(' ', 1)
         cab = i[subgroup][1]
 
-        if (lesson[0] != '') and (lesson[0] in descript):
+        if (lesson[0] != '') and (lesson[0].split('.')[0] in descript):
             para = f'''{lesson[1].ljust(tabs, " ")} {cab}'''
         else:
             para = f'''{lesson[0 ]+ ' '}{lesson[1].ljust(tabs, " ")}{cab}'''
