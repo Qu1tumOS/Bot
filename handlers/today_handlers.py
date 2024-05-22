b1635194b43a0f46963e09f1926e36e8b21ef85d
Qu1tum <vladimirzagotovkin@yandex.ru>
2024-05-22 22:12:11 +0300

    update requirements.txt and update today_handler

--- handlers/today_handlers.py
+++ handlers/today_handlers.py
@@ -7,6 +7,7 @@ from parser.lexicon_pars import print_day
 from keyboards.keyboard_creator import create_inline_kb
 
 from DataBase.db_connect import *
+import datetime
 
 
 router = Router()
@@ -14,9 +15,12 @@ router = Router()
 
 @router.callback_query(F.data == 'today_button')
 async def today(callback: CallbackQuery):
-    user = session.query(User).filter(User.tg_id==callback.from_user.id).first()
+    today = f'{datetime.today():%d.%m.%Y}'
 
-    today = date(0)
+    user = session.query(User).filter(User.tg_id==callback.from_user.id).first()
+    data = session.query(Lesson).filter(Lesson.day==today).first().lessons
+
+
     week_pars = group_par(user.group)
 
     if today in week_pars:
@@ -29,6 +33,31 @@ async def today(callback: CallbackQuery):
                                               menu_button='Назад',
                                               update_today='Обновить')
             )
+
+    elif data:
+        subgroup = int(user.subgroup) - 1
+
+        tabs = 24
+
+        if data:
+            output = [f'{today[:5].rjust(19, " ")}']
+            for i in data[user.group]:
+                para = i[subgroup][0]
+                cab = i[subgroup][1]
+
+                lesson = f'{para.ljust(tabs, " ")} {cab}'
+
+                output.append(lesson)
+            output = '\n'.join(output)
+
+            await callback.message.edit_text(
+            text=f'`{output}`',
+            parse_mode='MarkdownV2',
+            reply_markup=create_inline_kb(2,
+                                          menu_button='Назад',
+                                          tomorrow_button='Пары завтра'))
+            await callback.answer()
+
     else:
         await callback.message.edit_text(
             text=f'Сегодня пар уже не будет',
@@ -40,9 +69,12 @@ async def today(callback: CallbackQuery):
 
 @router.callback_query(F.data == 'update_today')
 async def update_today(callback: CallbackQuery):
-    user = session.query(User).filter(User.tg_id==callback.from_user.id).first()
+    today = f'{datetime.today():%d.%m.%Y}'
 
-    today = date(0)
+    user = session.query(User).filter(User.tg_id==callback.from_user.id).first()
+    data = session.query(Lesson).filter(Lesson.day==today).first().lessons
+
+
     week_pars = group_par(user.group)
 
     if today in week_pars:
@@ -56,6 +88,31 @@ async def update_today(callback: CallbackQuery):
             await callback.answer(text='Обновлено ✅')
         else:
             await callback.answer(text='Расписание не изменилось ✅')
+
+    elif data:
+        subgroup = int(user.subgroup) - 1
+
+        tabs = 24
+
+        if data:
+            output = [f'{today[:5].rjust(19, " ")}']
+            for i in data[user.group]:
+                para = i[subgroup][0]
+                cab = i[subgroup][1]
+
+                lesson = f'{para.ljust(tabs, " ")} {cab}'
+
+                output.append(lesson)
+            output = '\n'.join(output)
+
+            await callback.message.edit_text(
+            text=f'`{output}`',
+            parse_mode='MarkdownV2',
+            reply_markup=create_inline_kb(2,
+                                          menu_button='Назад',
+                                          next_day_par='Пары завтра'))
+            await callback.answer()
+
     else:
         await callback.message.edit_text(
             text='Сегодня пар уже не будет',
@@ -81,6 +138,9 @@ async def not_today(callback: CallbackQuery):
                                     menu_button='Назад',
                                     update_tomorrow='Обновить')
         )
+    elif session.query(Lesson).filter(Lesson.day==callback.data).first().lessons:
+        pass
+
     else:
         await callback.message.edit_text(
             text='завтра пар не будет',
